d1a5dbfb87fd48f910156aabbd6082d5247281b9
Ines Alvergne <i.alvergne@gmail.com>
2023-03-24 12:21:12 -0400

    email validation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello from src/index.js!");
+const valid = (email) => {
+  const regex = new RegExp(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/);
+  return regex.test(email);
+};
+
+const welcomeForm = document.querySelector("#welcome-form");
+welcomeForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const inputEmail = document.querySelector("#inputEmail");
+  const emailValidation = document.querySelector("#emailValidation");
+  if (valid(inputEmail.value) === false) {
+    emailValidation.classList.remove("d-none");
+    inputEmail.classList.add("not-valid");
+  }
+});
